fcac800cef23a8a5bb6092858f18eaec5f999855
Mahedul-Karim <karimrupo21@gmail.com>
2024-04-04 16:16:38 +0600

    table bug fixed

--- src/components/UI/table/TableBody.tsx
+++ src/components/UI/table/TableBody.tsx
@@ -12,7 +12,7 @@ type Props = {
 
 const TableBody: React.FC<Props> = function ({ type, cabin,booking }) {
   return (
-    <div className="grid grid-cols-[0.6fr_2fr_2.4fr_1.4fr_1fr_3.2rem] bg-grey-50 py-[1.2rem] px-[2.4rem] gap-x-[2.4rem] w-[938px] items-center !grid-rows-[35px]">
+    <div className="grid grid-cols-[0.6fr_2fr_2.4fr_1.4fr_1fr_3.2rem] bg-grey-50 py-[1.2rem] px-[2.4rem] gap-x-[2.4rem] min-w-[938px] items-center !grid-rows-[35px]">
       {type === "booking" && <BookingTable data={booking as Bookings}/>}
       {type === "cabin" && (
         <>
